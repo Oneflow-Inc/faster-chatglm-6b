3aeb576b1a561775462f87c0c4e3a432cc92b9f9
XIE Xuan <xiexuanx2@gmail.com>
2023-03-23 17:18:24 +0800

    examples (#7)
    * examples
    
    * add reference and format files

---
+++ examples/demo.py
@@ -0,0 +1,11 @@
+# This code is base on https://github.com/THUDM/ChatGLM-6B project
+
+import faster_chatglm_6b
+
+from transformers import AutoTokenizer, AutoModel
+
+tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
+model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
+model = model.eval()
+response, history = model.chat(tokenizer, "你好", history=[])
+print(response)

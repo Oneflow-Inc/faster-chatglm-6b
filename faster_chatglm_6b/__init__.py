77839b87cb35f7a1b2a467c9d1c66bff7076127c
liujuncheng <liujuncheng1022@gmail.com>
2023-04-12 14:51:33 +0800

    refine mock

--- faster_chatglm_6b/__init__.py
+++ faster_chatglm_6b/__init__.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ["ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT"] = "1"
 os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
 
@@ -9,20 +10,35 @@ from oneflow import nn
 
 flow.mock_torch.enable(lazy=True)
 
+
 def new_skip_init(module_cls, *args, **kwargs):
     return module_cls(*args, **kwargs)
 
-nn.utils.skip_init = new_skip_init
+
+if not hasattr(nn.utils, "skip_init"):
+    nn.utils.skip_init = new_skip_init
+
+
+def _tensor_data_ptr(self):
+    return id(self)
+
+
+if not hasattr(flow.Tensor, "data_ptr"):
+    flow.Tensor.data_ptr = _tensor_data_ptr
 
 from transformers import dynamic_module_utils
+
 orig_get_class_from_dynamic_module = dynamic_module_utils.get_class_from_dynamic_module
+
 
 def hook_get_class_from_dynamic_module(*args, **kwargs):
     _, _, cls_name = args
     if cls_name == "ChatGLMForConditionalGeneration":
         from .modeling_chatglm import ChatGLMForConditionalGeneration
+
         return ChatGLMForConditionalGeneration
     else:
         return orig_get_class_from_dynamic_module(*args, **kwargs)
+
 
 dynamic_module_utils.get_class_from_dynamic_module = hook_get_class_from_dynamic_module

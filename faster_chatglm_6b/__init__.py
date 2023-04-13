a62a29b14980604a957569eb37f15e191291d335
XIE Xuan <xiexuanx2@gmail.com>
2023-04-13 16:52:30 +0800

    Align with official chatglm-6b repository (#15)
    * init
    
    * add fused_attn
    
    * transform attention_mask to attn_bias
    
    * add requirements
    
    * mv attn_fn to SelfAttention
    
    * mv attn_fn into SelfAttention
    
    * rm pip requirements
    
    * only pad attn_bias's last dim
    
    * use skip layernorm
    
    * use fused_fast_gelu_mul
    
    * sync hf (#3)
    
    * sync hf
    
    * fix
    
    * add 8-bit quantization (#4)
    
    * add 8-bit quantization
    
    * add group_dim and group_size
    
    * copyright
    
    * fix
    
    * fused matmul bias (#5)
    
    * Update (#6)
    
    * sync hf
    
    * fix
    
    * update
    
    * fix
    
    * examples (#7)
    
    * examples
    
    * add reference and format files
    
    * update https://huggingface.co/THUDM/chatglm-6b/commit/4a9b711e61d62b64ae8a07d763553a98a984d281 (#8)
    
    * add 4 bit quantization (#11)
    
    * refine pack int8 to int4 (#13)
    
    * update README (#10)
    
    * update README
    
    * update
    
    * update install methods
    
    * update
    
    * update
    
    * rm install requirements
    
    * add link of oneflow
    
    * update setup and requirements (#12)
    
    * update setup and requirements
    
    * fix
    
    * remove vision
    
    * Add model revision (#14)
    
    * add model revision
    
    * [Fix context length in get_position_ids](https://huggingface.co/THUDM/chatglm-6b/commit/096f3de6b4959ce38bef7bb05f3129c931a3084e)
    
    * fix
    
    * update modeling_chatglm.py
    
    * update
    
    * quantization.py
    
    * update revision
    
    * Add rotary emb (#9)
    
    * fused_apply_rotary_emb v1
    
    * add MB(H3K) support
    
    * fix revision in demo files (#16)
    
    * update
    
    * update
    
    * update
    
    * update commit
    
    * rm unused files
    
    * update demos
    
    * update
    
    * update requirements
    
    * rm usless files
    
    * import torch
    
    * fix
    
    * add config
    
    * update
    
    * recover quantization.py
    
    * update
    
    ---------
    
    Co-authored-by: liujuncheng <liujuncheng1022@gmail.com>
    Co-authored-by: ofhwei <hwei1023@gmail.com>
    Co-authored-by: huangwei <109642856+ofhwei@users.noreply.github.com>

--- faster_chatglm_6b/__init__.py
+++ faster_chatglm_6b/__init__.py
@@ -3,13 +3,15 @@ import os
 os.environ["ONEFLOW_LINEAR_EMBEDDING_SKIP_INIT"] = "1"
 os.environ["ONEFLOW_KERNEL_ENABLE_FUSED_LINEAR"] = "1"
 
-revision = "096f3de6b4959ce38bef7bb05f3129c931a3084e"
+revision = "aa51e62ddc9c9f334858b0af44cf59b05c70148a"
 
 import oneflow as flow
 from oneflow import nn
 
 flow.mock_torch.enable(lazy=True)
 
+with flow.mock_torch.disable():
+    import torch
 
 def new_skip_init(module_cls, *args, **kwargs):
     return module_cls(*args, **kwargs)
@@ -20,9 +22,7 @@ if not hasattr(nn.utils, "skip_init"):
 
 
 from transformers import dynamic_module_utils
-
 orig_get_class_from_dynamic_module = dynamic_module_utils.get_class_from_dynamic_module
-
 
 def hook_get_class_from_dynamic_module(*args, **kwargs):
     _, _, cls_name = args
